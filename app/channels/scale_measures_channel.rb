514d519f27776f3d5abfaa28f38c6ea3861892f7
Fabio Lima Gainzarain <fabio.lima@moove-it.com>
2020-04-19 15:47:51 -0300

    configuraciones para deploy

--- app/channels/scale_measures_channel.rb
+++ app/channels/scale_measures_channel.rb
@@ -1,8 +1,5 @@
-class ScaleMeasureChannel < ApplicationCable::Channel
+class ScaleMeasuresChannel < ApplicationCable::Channel
   def subscribed
-    puts "\n\n\n\n\n\n\n"
-    puts params
-    puts "\n\n\n\n\n\n\n\n"
     scale_measure = ScaleMeasure.find(params[:id])
     stream_from 'scale_measures'
   end
