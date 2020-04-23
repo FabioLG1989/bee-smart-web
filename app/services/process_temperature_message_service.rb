b4df66f99bd50f036ad1431f954ed4f1c3045644
Fabio Lima Gainzarain <fabio.lima@moove-it.com>
2020-04-23 16:21:55 -0300

    add resource retrieval for scale calibration

--- app/services/process_temperature_message_service.rb
+++ app/services/process_temperature_message_service.rb
@@ -15,6 +15,9 @@ class ProcessTemperatureMessageService < ApplicationService
     sensors_data.each do |sensor_data|
       sensor_uuid = "#{@hive.uuid}:#{i}"
       if sensor_data != 0 && sensor_data.to_i != 127
+        integer, decimal = sensor_data.split('.')
+        decimal = "%.2f" % (decimal.to_i * 10.0/16)
+        sensor_data = [integer, decimal].join('.')
         sensor = TemperatureSensor.find_or_create_by(uuid: sensor_uuid)
         sensor.update!(temperature_grid: @hive.temperature_grid) unless sensor.temperature_grid
         TemperatureMeasure.create!(temperature_sensor: sensor, temperature: sensor_data, measured_at: @date)
