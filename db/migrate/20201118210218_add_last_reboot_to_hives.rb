70add61bc5ec841ce258de47918e2630a91be8ff
Fabio Lima Gainzarain <fabio.lima@moove-it.com>
2020-11-18 18:14:16 -0300

    Agregar watchdog para resetar ante fallo en grilla

--- db/migrate/20201118210218_add_last_reboot_to_hives.rb
+++ db/migrate/20201118210218_add_last_reboot_to_hives.rb
@@ -1,5 +1,5 @@
 class AddLastRebootToHives < ActiveRecord::Migration[6.0]
   def change
-    add_column :hives, :last_reboot, default: Time.current
+    add_column :hives, :last_reboot, :datetime, default: Time.current
   end
 end
